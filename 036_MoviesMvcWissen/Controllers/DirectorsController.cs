32ab2fffcaf01a8f835521abed646b0eb4d5ecb9
Section2 <Section2@DESKTOP-KHOPMPR>
2020-02-11 21:18:12 +0300

    11.02.2020

--- 036_MoviesMvcWissen/Controllers/DirectorsController.cs
+++ 036_MoviesMvcWissen/Controllers/DirectorsController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using _036_MoviesMvcWissen.Contexts;
 using _036_MoviesMvcWissen.Entities;
+using _036_MoviesMvcWissen.Models.ViewModels;
 
 namespace _036_MoviesMvcWissen.Controllers
 {
@@ -19,7 +20,12 @@ namespace _036_MoviesMvcWissen.Controllers
         // GET: Directors
         public ActionResult Index()
         {
-            return View(db.Directors.ToList());
+            var model = new DirectorsIndexViewModel()
+            {
+                DirectorsList=db.Directors.ToList()
+            };
+            return View(model);
+            //return View(db.Directors.ToList())
         }
 
         // GET: Directors/Details/5
@@ -40,6 +46,12 @@ namespace _036_MoviesMvcWissen.Controllers
         // GET: Directors/Create
         public ActionResult Create()
         {
+            var movies = db.Movies.ToList().Select(e => new SelectListItem() //directors listesini aldık ve listbox da kullanabilmek için multiselectliste çevirdik
+            {
+                Value = e.Id.ToString(),
+                Text = e.Name 
+            }).ToList();
+            ViewData["movies"] = new MultiSelectList(movies, "Value", "Text");
             return View();
         }
 
@@ -50,33 +62,41 @@ namespace _036_MoviesMvcWissen.Controllers
         [ValidateAntiForgeryToken]
         //public ActionResult Create([Bind(Include = "Id,Name,Surname,Retired")] Director director)
         [ActionName("Create")]
-        public ActionResult CreateNew() //public ActionResult CreateNew(FormCollection formCollection)
+        //public ActionResult CreateNew() //public ActionResult CreateNew(FormCollection formCollection)
+        public ActionResult Create([Bind(Include = "Id,Name,Surname,Retired")] Director director,List<int>movieIds)
         {
-            var director = new Director()
+            var newDirector = new Director()
             {
-                Name = Request.Form["Name"],
-                Surname = Request.Form["Surname"],
+                Name = director.Name,
+                Surname = director.Surname,
                 //Name = formCollection["Name"];
                 //Surname = formCollection["Surname"];
                
                 
             };
             //var retired = formCollection["Retired"];
-            var retired = Request.Form["Retired"];
-            director.Retired = true;
-            if (retired.Equals("false"))
-                director.Retired = false;
-            if (String.IsNullOrWhiteSpace(director.Name))
-                ModelState.AddModelError("Name", "Director Name is required!");
-            if (String.IsNullOrWhiteSpace(director.Surname))
-                ModelState.AddModelError("Surname", "Director Surname is required!");
-            if(director.Name.Length>100)
-                ModelState.AddModelError("Name","Director Name must be maximum 100 characters");
-            if(director.Surname.Length>100)
-                ModelState.AddModelError("Surname","Director Surname must be maximum 100 characters");
+            var retired = director.Retired;
+            newDirector.Retired = retired;
+            newDirector.MovieDirectors = movieIds.Select(e => new MovieDirector()
+            {
+                MovieId = e,
+                DirectorId = director.Id
+            }).ToList();
+
+            
+            //if (newDirector.Equals("false"))
+            //    director.Retired = false;
+            //if (String.IsNullOrWhiteSpace(newDirector.Name))
+            //    ModelState.AddModelError("Name", "Director Name is required!");
+            //if (String.IsNullOrWhiteSpace(newDirector.Surname))
+            //    ModelState.AddModelError("Surname", "Director Surname is required!");
+            //if(newDirector.Name.Length>100)
+            //    ModelState.AddModelError("Name","Director Name must be maximum 100 characters");
+            //if(newDirector.Surname.Length>100)
+            //    ModelState.AddModelError("Surname","Director Surname must be maximum 100 characters");
             if (ModelState.IsValid)
             {
-                db.Directors.Add(director);
+                db.Directors.Add(newDirector);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
@@ -96,6 +116,9 @@ namespace _036_MoviesMvcWissen.Controllers
             {
                 return HttpNotFound();
             }
+            var movies = db.Movies.ToList();
+            var movieIds = db.Directors.Find(id).MovieDirectors.Select(e => e.MovieId).ToList();
+            ViewBag.movies = new MultiSelectList(movies, "Id","Name", movieIds);
             return View(director);
         }
 
@@ -104,11 +127,36 @@ namespace _036_MoviesMvcWissen.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,Surname,Retired")] Director director)
+        public ActionResult Edit([Bind(Include = "Id,Name,Surname,Retired")] Director director,List<int> movieIds)
         {
+            var entity = db.Directors.SingleOrDefault(e => e.Id == director.Id);
+            entity.Name = director.Name;
+            entity.Retired = director.Retired;
+            entity.Surname = director.Surname;
+            entity.MovieDirectors = new List<MovieDirector>();
+            var movieDirectors = db.MovieDirectors.Where(e => e.DirectorId == director.Id);
+            foreach (var movieDirector in movieDirectors)
+            {
+                db.MovieDirectors.Remove(movieDirector); // movienin yönetmenlerini sildik ara tabloyu bu moviele alakalı verileri boşalttık
+            }
+            //alttaki foreachle aynı işi yapar
+            //entity.MovieDirectors = movieIds.Select(e => new MovieDirector() 
+            //{
+            //    MovieId=e,
+            //    DirectorId=director.Id
+            //}).ToList();
+            foreach (var movieId in movieIds) // elimizdeki editlenecek yönetmenlerin idlerine göre her biri için movie director newleyerek entitymize ekleyip sonrasında entityi veritabanında güncelledik
+            {
+                var movieDirector = new MovieDirector()
+                {
+                    DirectorId = director.Id,
+                    MovieId = movieId
+                };
+                entity.MovieDirectors.Add(movieDirector);
+            }
             if (ModelState.IsValid)
             {
-                db.Entry(director).State = EntityState.Modified;
+                db.Entry(entity).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
