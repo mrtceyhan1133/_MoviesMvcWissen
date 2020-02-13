6cd2aeb98a0fabb22ff7ebbc707ae2617109204f
Section2 <Section2@DESKTOP-KHOPMPR>
2020-02-13 21:17:32 +0300

    13.02.2020

--- 036_MoviesMvcWissen/Contexts/MoviesContext.cs
+++ 036_MoviesMvcWissen/Contexts/MoviesContext.cs
@@ -20,6 +20,6 @@ namespace _036_MoviesMvcWissen.Contexts
         public virtual DbSet<Review> Reviews { get; set; }
         public virtual DbSet<MovieDirector> MovieDirectors { get; set; }
 
-        public System.Data.Entity.DbSet<_036_MoviesMvcWissen.Models.Demos.Templates.Person> People { get; set; }
+       
     }
 }
--- 036_MoviesMvcWissen/Controllers/DemosController.cs
+++ 036_MoviesMvcWissen/Controllers/DemosController.cs
@@ -43,16 +43,19 @@ namespace _036_MoviesMvcWissen.Controllers
         {
             var name = Request.QueryString["name"];
             var surname = Request.QueryString["surname"];
-            
+
             return name + " " + surname;
         }
         #endregion
         #region Templates
         public ActionResult GetPeople()
         {
-            List<Person> people = new List<Person>()
+            List<PersonModel> people;
+            if (Session["people"] == null)
             {
-                new Person()
+                people = new List<PersonModel>()
+                {
+                new PersonModel()
                 {
                     Id=1,
                     FullName="Çağıl Alsaç",
@@ -60,7 +63,7 @@ namespace _036_MoviesMvcWissen.Controllers
                     GraduateFromUniversity=true,
                     BirthDate=DateTime.Parse("19.06.1980")
                 },
-                new Person()
+                new PersonModel()
                 {
                     Id=2,
                     FullName="Leo",
@@ -68,15 +71,37 @@ namespace _036_MoviesMvcWissen.Controllers
                     GraduateFromUniversity=false,
                     BirthDate=DateTime.Parse("25.05.2015")
                 }
-            };
-            Session["people"] = people;
+
+                };
+                Session["people"] = people;
+            }
+            else
+            {
+                people = Session["people"] as List<PersonModel>;
+            }
             return View(people);
         }
+
         public ActionResult GetPersonDetails(int? id)
         {
-            List<Person> people = Session["people"] as List<Person>;
-            Person person = people.SingleOrDefault(e => e.Id == id);
+            List<PersonModel> people = Session["people"] as List<PersonModel>;
+            PersonModel person = people.SingleOrDefault(e => e.Id == id);
             return View(person);
+        }
+        public ActionResult AddPerson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddPerson(PersonModel personModel)
+        {
+            List<PersonModel> people = Session["people"] as List<PersonModel>;
+            personModel.Id = people.Max(e => e.Id) + 1;
+            people.Add(personModel);
+            Session["people"] = people;
+            return RedirectToAction("GetPeople");
+
         }
         #endregion
     }
