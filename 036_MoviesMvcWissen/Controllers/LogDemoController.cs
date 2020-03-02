6f0644bb31e11f41b4150e05ae1c81ec4e91e0a0
Section2 <Section2@DESKTOP-KHOPMPR>
2020-03-02 21:21:24 +0300

    02,03,2020

---
+++ 036_MoviesMvcWissen/Controllers/LogDemoController.cs
@@ -0,0 +1,25 @@
+﻿using _036_MoviesMvcWissen.Models.LogDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _036_MoviesMvcWissen.Controllers
+{
+    public class LogDemoController : Controller
+    {
+        private ILogger _logger;
+        public LogDemoController(ILogger logger)
+        {
+            _logger = logger;
+        }
+        // GET: LogDemo
+        public ActionResult Index()
+        {
+            //_logger = new DatabaseLogger(); dependincy inj yapılmaması gereken
+            _logger.Log("Home Controller -> Index Action executed.");
+            return Content("Home Controller->Index Action executed.");
+        }
+    }
+}
